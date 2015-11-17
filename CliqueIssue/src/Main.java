c5ba066f2531db21e2a2312c2061bad9434f311c
jfourmond <jerome@fourmond.fr>
2015-11-17 20:41:20 +0100

    Correction

--- CliqueIssue/src/Main.java
+++ CliqueIssue/src/Main.java
@@ -2,11 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		Graphe G = new Graphe(2);
 		G.init();
 		System.out.println(G);
+		*/
 		
 		Graphe G1 = new Graphe("./src/petitGraphe.clq");
-	
+		System.out.println(G1);
 	}
 }
