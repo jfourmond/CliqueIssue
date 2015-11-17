a6a2f4a77de42598f9724ea59de000fe89af53ad
jfourmond <jerome@fourmond.fr>
2015-11-17 18:43:03 +0100

    correction

--- CliqueIssue/src/Main.java
+++ CliqueIssue/src/Main.java
@@ -4,11 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Coucou !");
-		
-<<<<<<< HEAD
-		System.out.println("Conflict !");
-=======
-		System.out.println("Autre chose");
->>>>>>> branch 'master' of https://github.com/jfourmond/CliqueIssue.git
+	
 	}
 }
