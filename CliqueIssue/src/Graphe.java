008d49fac6281e914c065955f6503ae886fc20c5
jfourmond <jerome@fourmond.fr>
2015-11-25 19:24:17 +0100

    conflict

--- CliqueIssue/src/Graphe.java
+++ CliqueIssue/src/Graphe.java
@@ -388,6 +388,7 @@ public class Graphe {
 			System.out.println("Voisins de " + sommet + " : " + candidats);
 			return;
 		}
+		
 		if(P.isEmpty() && X.isEmpty()) {
 			System.out.println("Clique Maximal");
 			return;
